f70bf7916935394b6c6863220ee4f1898e35ec40
Mike Boutin <DWboutin@users.noreply.github.com>
2019-10-07 16:50:43 -0400

    iframe mailchimp

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,29 +1,10 @@
 import React from 'react'
-import { FormattedMessage } from 'react-intl'
-
-import Layout from '../components/Layout'
-import ProductListing from '../components/ProductListing'
-import ProductWithCategoriesListing from '../components/ProductWithCategoriesListing'
-import SEO from '../components/Seo'
-import SliderSection from '../components/SliderSection'
-import SmallProductListing from '../components/SmallProductListing'
-import { PageContext } from '../models/general/PagesDefinition'
 
 const IndexPage = ({ pageContext }: { pageContext: PageContext }) => (
-  <Layout>
-    <SEO
-      lang={pageContext.locale}
-      title="Home"
-      keywords={['gatsby', 'application', 'react']}
-    />
-    <SliderSection />
-    <ProductListing />
-    <SmallProductListing />
-    <ProductWithCategoriesListing />
-    <h1>
-      <FormattedMessage id="string1" />
-    </h1>
-  </Layout>
+  <iframe
+    src="https://mailchi.mp/54a9b74d0558/sub4open"
+    style={{ width: '100%', height: '100%' }}
+  ></iframe>
 )
 
 export default IndexPage
